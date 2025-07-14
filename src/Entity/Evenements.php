6740865c7a14a6359ee2dc86d8343f0dd6d60600
Adibada <adrienmansouri@gmail.com>
2025-07-14 15:54:40 +0200

    création et  migration de la première entité

---
+++ src/Entity/Evenements.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EvenementsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EvenementsRepository::class)]
+class Evenements
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column(type: 'text')]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $organisateur = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $start = null;
+
+    #[ORM\Column(type: Types::DATE_IMMUTABLE)]
+    private ?\DateTimeImmutable $end = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lot = null;
+
+    #[ORM\Column]
+    private ?int $competitors = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $statut = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getOrganisateur(): ?string
+    {
+        return $this->organisateur;
+    }
+
+    public function setOrganisateur(string $organisateur): static
+    {
+        $this->organisateur = $organisateur;
+
+        return $this;
+    }
+
+    public function getStart(): ?\DateTimeImmutable
+    {
+        return $this->start;
+    }
+
+    public function setStart(\DateTimeImmutable $start): static
+    {
+        $this->start = $start;
+
+        return $this;
+    }
+
+    public function getEnd(): ?\DateTimeImmutable
+    {
+        return $this->end;
+    }
+
+    public function setEnd(\DateTimeImmutable $end): static
+    {
+        $this->end = $end;
+
+        return $this;
+    }
+
+    public function getLot(): ?string
+    {
+        return $this->lot;
+    }
+
+    public function setLot(string $lot): static
+    {
+        $this->lot = $lot;
+
+        return $this;
+    }
+
+    public function getCompetitors(): ?int
+    {
+        return $this->competitors;
+    }
+
+    public function setCompetitors(int $competitors): static
+    {
+        $this->competitors = $competitors;
+
+        return $this;
+    }
+
+    public function getStatut(): ?string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(string $statut): static
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
